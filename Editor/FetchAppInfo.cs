8513ca32b6934ad44021bcc881d07906e45c4c75
Emre Kovanci <kovanciemre@gmail.com>
2022-03-12 18:30:33 +0300

    Update unity api

--- Editor/FetchAppInfo.cs
+++ Editor/FetchAppInfo.cs
@@ -15,7 +15,7 @@ namespace TalusCI.Editor
             using UnityWebRequest www = UnityWebRequest.Get($"{API_URL}/{GetProjectName()}");
             yield return www.SendWebRequest();
 
-            if (www.isNetworkError || www.isHttpError)
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
             {
                 Debug.LogError(www.error);
             }
