3fe7171fa488859744bcfcdf7c63a0b7d91a537a
Emre Kovanci <kovanciemre@gmail.com>
2022-04-21 20:39:06 +0300

    Revert "remove unnec file"
    This reverts commit 4413df5cfeb922495834489bf996cbc16a30ec2e.

---
+++ Editor/iOS/ExemptFromEncryption.cs
@@ -0,0 +1,31 @@
+#if UNITY_IOS
+using System.IO;
+
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+using UnityEditor.iOS.Xcode;
+
+namespace TalusCI.Editor.iOS
+{
+    public class ExemptFromEncryption : UnityEditor.Editor
+    {
+        [PostProcessBuild(9998)]
+        public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+
+        {
+            if (buildTarget != BuildTarget.iOS && buildTarget != BuildTarget.tvOS) return;
+
+            string plistPath = path + "/Info.plist";
+
+            var plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
+
+            PlistElementDict rootDict = plist.root;
+            rootDict.SetBoolean("ITSAppUsesNonExemptEncryption", false);
+
+            File.WriteAllText(plistPath, plist.WriteToString());
+        }
+    }
+}
+#endif
