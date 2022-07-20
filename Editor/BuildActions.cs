9efec535638c56f9293431080039c1ad223dc58c
Emre Kovanci <kovanciemre@gmail.com>
2022-07-21 00:18:08 +0300

    export class

--- Editor/BuildActions.cs
+++ Editor/BuildActions.cs
@@ -1,14 +1,4 @@
-using System.IO;
-using System.Linq;
-
 using UnityEditor;
-using UnityEditor.Build.Reporting;
-using UnityEngine;
-
-using TalusCI.Editor.Utility;
-
-using TalusBackendData.Editor;
-using TalusBackendData.Editor.Models;
 
 namespace TalusCI.Editor
 {
@@ -17,91 +7,47 @@ namespace TalusCI.Editor
         [MenuItem("TalusBackend/Manuel Build/iOS Development", priority = 11000)]
         public static void IOSDevelopment()
         {
-            PrepareIOSBuild(true);
+            BuildCreator buildInfo = new BuildCreator
+            {
+                IsDebugBuild = true,
+                TargetPlatform = BuildTarget.iOS,
+                TargetGroup = BuildTargetGroup.iOS
+            };
+            buildInfo.PrepareBuild();
         }
 
         [MenuItem("TalusBackend/Manuel Build/iOS Release", priority = 11001)]
         public static void IOSRelease()
         {
-            PrepareIOSBuild(false);
+            BuildCreator buildInfo = new BuildCreator
+            {
+                TargetPlatform = BuildTarget.iOS,
+                TargetGroup = BuildTargetGroup.iOS
+            };
+            buildInfo.PrepareBuild();
         }
 
-        private static void PrepareIOSBuild(bool isDevelopment)
+        [MenuItem("TalusBackend/Manuel Build/Android Development", priority = 11002)]
+        public static void AndroidDevelopment()
         {
-            EditorUserBuildSettings.development = isDevelopment;
-
-            bool isBatchMode = Application.isBatchMode;
-
-            if (!isBatchMode && EditorUserBuildSettings.activeBuildTarget != BuildTarget.iOS)
+            BuildCreator buildInfo = new BuildCreator
             {
-                Debug.LogError("[TalusCI-Package] Build Target must be iOS! Switch platform to iOS (File/Build Settings)");
-                return;
-            }
-
-            string apiUrl = (isBatchMode)
-                ? CommandLineParser.GetArgument("-apiUrl")
-                : BackendSettingsHolder.instance.ApiUrl;
-
-            string apiToken = (isBatchMode)
-                ? CommandLineParser.GetArgument("-apiKey")
-                : BackendSettingsHolder.instance.ApiToken;
-
-            string appId = (isBatchMode)
-                ? CommandLineParser.GetArgument("-appId")
-                : BackendSettingsHolder.instance.AppId;
-
-            // create build when backend data fetched
-            BackendApi api = new BackendApi(apiUrl, apiToken);
-            api.GetAppInfo(appId, CreateBuild);
+                TargetPlatform = BuildTarget.Android,
+                TargetGroup = BuildTargetGroup.Android
+            };
+            buildInfo.PrepareBuild();
         }
 
-        private static void CreateBuild(AppModel app)
+        [MenuItem("TalusBackend/Manuel Build/Android Release", priority = 11002)]
+        public static void AndroidRelease()
         {
-            Debug.Log($"[TalusCI-Package] Define Symbols: {PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS)}");
-
-            UpdateProductSettings(app);
-
-            string buildPath = Path.Combine(CISettingsHolder.ProjectFolder, CISettingsHolder.instance.BuildFolder);
-            Debug.Log($"[TalusCI-Package] Build path: {buildPath}");
-
-            BuildReport report = BuildPipeline.BuildPlayer(
-                GetScenes(),
-                buildPath,
-                BuildTarget.iOS,
-                BuildOptions.CompressWithLz4HC
-            );
-
-            Debug.Log($"[TalusCI-Package] Build status: {report.summary.result}");
-            Debug.Log($"[TalusCI-Package] Output path: {report.summary.outputPath}");
-
-            // batch mode clean-up
-            if (!Application.isBatchMode)
+            BuildCreator buildInfo = new BuildCreator
             {
-                return;
-            }
-
-            EditorApplication.Exit(report.summary.result == BuildResult.Succeeded ? 0 : -1);
+                IsDebugBuild = false,
+                TargetPlatform = BuildTarget.Android,
+                TargetGroup = BuildTargetGroup.Android
+            };
+            buildInfo.PrepareBuild();
         }
-
-        private static void UpdateProductSettings(AppModel app)
-        {
-            PlayerSettings.SplashScreen.showUnityLogo = false;
-            PlayerSettings.SetScriptingBackend(BuildTargetGroup.iOS, ScriptingImplementation.IL2CPP);
-
-            if (app != null)
-            {
-                PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.iOS, app.app_bundle);
-                PlayerSettings.productName = app.app_name;
-            }
-            else
-            {
-                Debug.LogError($"[TalusCI-Package] AppModel data is null! Product Settings couldn't updated...");
-            }
-
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
-        }
-
-        private static string[] GetScenes() => (from t in EditorBuildSettings.scenes select t.path).ToArray();
     }
 }
---
+++ Editor/BuildCreator.cs
@@ -0,0 +1,98 @@
+using System.IO;
+using System.Linq;
+
+using UnityEngine;
+
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+using TalusBackendData.Editor.Models;
+using TalusBackendData.Editor;
+
+using TalusCI.Editor.Utility;
+
+namespace TalusCI.Editor
+{
+    public class BuildCreator
+    {
+        public string[] Scenes => (from t in EditorBuildSettings.scenes select t.path).ToArray();
+
+        public bool IsDebugBuild;
+
+        public BuildTarget TargetPlatform;
+        public BuildTargetGroup TargetGroup;
+        public BuildOptions Options = BuildOptions.CompressWithLz4HC;
+
+        public void PrepareBuild()
+        {
+            EditorUserBuildSettings.development = IsDebugBuild;
+
+            bool isBatchMode = Application.isBatchMode;
+
+            if (!isBatchMode && !(EditorUserBuildSettings.activeBuildTarget != BuildTarget.iOS ||
+                EditorUserBuildSettings.activeBuildTarget != BuildTarget.Android))
+            {
+                Debug.LogError("[TalusCI-Package] Build Target must be iOS/Android! Switch platform (File/Build Settings)");
+                return;
+            }
+
+            string apiUrl = (isBatchMode)
+                ? CommandLineParser.GetArgument("-apiUrl")
+                : BackendSettingsHolder.instance.ApiUrl;
+
+            string apiToken = (isBatchMode)
+                ? CommandLineParser.GetArgument("-apiKey")
+                : BackendSettingsHolder.instance.ApiToken;
+
+            string appId = (isBatchMode)
+                ? CommandLineParser.GetArgument("-appId")
+                : BackendSettingsHolder.instance.AppId;
+
+            // create build when backend data fetched
+            BackendApi api = new BackendApi(apiUrl, apiToken);
+            api.GetAppInfo(appId, CreateBuild);
+        }
+
+        private void CreateBuild(AppModel app)
+        {
+            Debug.Log($"[TalusCI-Package] Define Symbols: {PlayerSettings.GetScriptingDefineSymbolsForGroup(TargetGroup)}");
+
+            UpdateProductSettings(app);
+
+            string buildPath = Path.Combine(CISettingsHolder.ProjectFolder, CISettingsHolder.instance.BuildFolder);
+            Debug.Log($"[TalusCI-Package] Build path: {buildPath}");
+
+            BuildReport report = BuildPipeline.BuildPlayer(Scenes, buildPath, TargetPlatform, Options);
+
+            Debug.Log($"[TalusCI-Package] Build status: {report.summary.result}");
+            Debug.Log($"[TalusCI-Package] Output path: {report.summary.outputPath}");
+
+            // batch mode clean-up
+            if (!Application.isBatchMode)
+            {
+                return;
+            }
+
+            EditorApplication.Exit(report.summary.result == BuildResult.Succeeded ? 0 : -1);
+        }
+
+        private void UpdateProductSettings(AppModel app)
+        {
+            PlayerSettings.SplashScreen.showUnityLogo = false;
+            PlayerSettings.SetScriptingBackend(TargetGroup, ScriptingImplementation.IL2CPP);
+
+            if (app != null)
+            {
+                PlayerSettings.SetApplicationIdentifier(TargetGroup, app.app_bundle);
+                PlayerSettings.productName = app.app_name;
+            }
+            else
+            {
+                Debug.LogError($"[TalusCI-Package] AppModel data is null! Product Settings couldn't updated...");
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
