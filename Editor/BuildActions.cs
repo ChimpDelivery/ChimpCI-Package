1a5754543566bdb2c46ba1ac7dc73cccf76a0493
Emre Kovanci <kovanciemre@gmail.com>
2022-11-01 08:56:52 +0300

    up

--- Editor/BuildActions.cs
+++ Editor/BuildActions.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 using TalusBackendData.Editor.Utility;
 
+using TalusCI.Editor.BuildSystem;
+
 namespace TalusCI.Editor
 {
     /// <summary>
@@ -10,16 +12,33 @@ namespace TalusCI.Editor
     /// </summary>
     public static class BuildActions
     {
+        private const string PackagePath = "Packages/com.talus.talusci/Editor/BuildSystem/BuildGenerators";
+
+        private const string iOSReleaseConfigs = "BuildGenerator_iOS_Release.asset";
+        private const string AndroidReleaseAABConfigs = "BuildGenerator_Android_Release_AAB.asset";
+        private const string AndroidReleaseAPKConfigs = "BuildGenerator_Android_Release_APK.asset";
+
+        [MenuItem("TalusBackend/Manuel Build/iOS/Release")]
         public static void IOSRelease()
         {
-            
+            string configPath = $"{PackagePath}/{iOSReleaseConfigs}";
+            LoadAndRun(configPath);
         }
 
+        [MenuItem("TalusBackend/Manuel Build/Android/Release(aab)")]
         public static void AndroidRelease()
         {
-            
+            string configPath = $"{PackagePath}/{AndroidReleaseAABConfigs}";
+            LoadAndRun(configPath);
         }
-        
+
+        [MenuItem("TalusBackend/Manuel Build/Android/Release(apk)")]
+        public static void AndroidReleaseAPK()
+        {
+            string configPath = $"{PackagePath}/{AndroidReleaseAPKConfigs}";
+            LoadAndRun(configPath);
+        }
+
         public static void SetBuildVersion()
         {
             string appVersion = CommandLineParser.GetArgument("-buildVersion");
@@ -33,6 +52,12 @@ namespace TalusCI.Editor
             Debug.Log("[TalusCI-Package] Version settings initialized.");
 
             EditorApplication.Exit(0);
+        }
+        
+        private static void LoadAndRun(string path)
+        {
+            var generator = AssetDatabase.LoadAssetAtPath<BuildGenerator>(path);
+            generator.Run();
         }
     }
 }
