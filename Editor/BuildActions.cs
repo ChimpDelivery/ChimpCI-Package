9696482d091794df2b5eb417a1dc8d7794481c48
Emre Kovanci <kovanciemre@gmail.com>
2022-11-01 07:53:00 +0300

    up

--- Editor/BuildActions.cs
+++ Editor/BuildActions.cs
@@ -9,34 +9,6 @@ namespace TalusCI.Editor
 {
     public static class BuildActions
     {
-        [MenuItem("TalusBackend/Manuel Build/iOS/Development", priority = 11000)]
-        public static void IOSDevelopment()
-        {
-            // new BuildCreator(true, BuildTarget.iOS, BuildTargetGroup.iOS);
-        }
-
-        [MenuItem("TalusBackend/Manuel Build/iOS/Release", priority = 11001)]
-        public static void IOSRelease()
-        {
-            // new BuildCreator(false, BuildTarget.iOS, BuildTargetGroup.iOS);
-        }
-
-        [MenuItem("TalusBackend/Manuel Build/Android/Development", priority = 11002)]
-        public static void AndroidDevelopment()
-        {
-            EditorUserBuildSettings.buildAppBundle = true;
-
-            // new BuildCreator(true, BuildTarget.Android, BuildTargetGroup.Android);
-        }
-
-        [MenuItem("TalusBackend/Manuel Build/Android/Release", priority = 11002)]
-        public static void AndroidRelease()
-        {
-            EditorUserBuildSettings.buildAppBundle = true;
-
-            // new BuildCreator(false, BuildTarget.Android, BuildTargetGroup.Android);
-        }
-
         // using by Jenkins
         public static void SetBuildVersion()
         {
