07c68f6d7ac4d3e7b83156d966de68aacee0c2e7
Emre Kovanci <kovanciemre@gmail.com>
2022-12-01 01:51:48 +0300

    first

---
+++ Editor/BuildSystem/BuildSteps/iOSProvision.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using TalusBackendData.Editor;
+using TalusBackendData.Editor.Requests;
+
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once InconsistentNaming
+
+namespace TalusCI.Editor.BuildSystem.BuildSteps
+{
+    [CreateAssetMenu(menuName = "_OTHERS/Build/Build Steps/iOS Provision")]
+    public class iOSProvision : BuildStep
+    {
+        private BackendApiConfigs _ApiConfigs;
+        
+        public override void Execute()
+        {
+            if (EditorUserBuildSettings.activeBuildTarget != BuildTarget.iOS) { return; }
+
+            var request = new ProvisionProfileRequest();
+            BackendApi.DownloadFile(request, onDownloadComplete: path =>
+            {
+                string provisionFileUuid = request.GetHeader(_ApiConfigs.ProvisionUuidKey);
+
+                Debug.Log($"[TalusCI-Package] iOSProvision Step | Provision profile path: {path}");
+                Debug.Log($"[TalusCI-Package] iOSProvision Step | Provision profile uuid: {provisionFileUuid}");
+                
+                PlayerSettings.iOS.iOSManualProvisioningProfileType = ProvisioningProfileType.Distribution;
+                PlayerSettings.iOS.iOSManualProvisioningProfileID = provisionFileUuid;
+            });
+            
+            Debug.Log("[TalusCI-Package] iOSProvision Step | Completed!");
+        }
+    }
+}
