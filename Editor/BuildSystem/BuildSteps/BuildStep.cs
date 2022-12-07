24e3ffb4c0338368b728af4997531cfa95faa51b
Emre Kovanci <kovanciemre@gmail.com>
2022-12-07 15:25:53 +0300

    up

--- Editor/BuildSystem/BuildSteps/BuildStep.cs
+++ Editor/BuildSystem/BuildSteps/BuildStep.cs
@@ -4,9 +4,6 @@ namespace TalusCI.Editor.BuildSystem.BuildSteps
 {
     public abstract class BuildStep : ScriptableObject
     {
-#if ODIN_INSPECTOR_3_1
-        [Sirenix.OdinInspector.Button]
-#endif
         public abstract void Execute();
     }
 }
